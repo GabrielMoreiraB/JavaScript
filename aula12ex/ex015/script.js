21b3d1ac9fe488f5a67fb8b6e6d125c3d9ded75e
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2022-10-22 11:47:05 -0300

    v

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,14 +1,57 @@
 function verificar(){
+
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if(fano.value.length == 0 || fano.value > ano){
-        window.alert('ERRO Verifique os dados e tente novamente')
-    } else {
-        window.alert('Tudo ok!')
-    }
 
+    if(fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            gênero = 'Homem'
+            if(idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'menino.png')
+            } else if (idade < 21 ) {
+                //jovem
+                img.setAttribute('src', 'jovem m.png')
+            } else if ( idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'homem.png')
+            }else {
+                //Idoso
+                img.setAttribute('src', 'idoso m.png')
+            }
+        } else {
+            gênero = 'Mulher'
+            document.body.style.background = 'pink'
+            if(idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'menina.png')
+            } else if (idade < 21 ) {
+                //jovem
+                img.setAttribute('src', 'jovem f.png')
+            } else if ( idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'mulher.png')
+            }else {
+                //Idoso
+                img.setAttribute('src', 'idoso f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com idade ${idade} anos`
+        res.appendChild(img)
+
+
+
+    }
+    
 
 
 
