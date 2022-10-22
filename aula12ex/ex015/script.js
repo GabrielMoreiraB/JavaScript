031557780d0765cdc8f901215ca5bd7c2729aedc
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2022-10-22 09:51:31 -0300

    adc imagens

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,8 +1,13 @@
-function clicar(){
-
-var ano = window.document.getElementById('ano')
-
-
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert('ERRO Verifique os dados e tente novamente')
+    } else {
+        window.alert('Tudo ok!')
+    }
 
 
 
