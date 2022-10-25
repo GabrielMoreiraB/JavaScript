17cf259deaf338fea2c7ee7ab24fbcf12878719d
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2022-10-25 19:32:52 -0300

    function adicionar feito

--- aula16 ex/ex 18/script.js
+++ aula16 ex/ex 18/script.js
@@ -1,30 +1,26 @@
 
-    var num = window.document.getElementById('fnum')
-    var lista = document.getElementById('flista')
+    let num = window.document.getElementById('fnum')
+    let lista = document.getElementById('flista')
     let res = document.querySelector('div#res')
     let valores = []
-
+    
 function isNumero (n) {
     if (Number(n) > 0 && Number(n) <= 100){
     return true
 }else {
     return false
-}
-}
+}}
 function inLista(n, l){
     if(l.indexOf(n) != -1) {
         return false
     }else {
     return true
-}
-}
-
-
-
-
+}}
 
 function adicionar () {
-    if (isNumero(num.value) && inLista(num.value, valores)){
+    
+    if (isNumero(num.value) && inLista(Number(num.value), valores))
+    {
     valores.push(Number(num.value)) 
     let item = document.createElement('option')
     item.text = `Valor ${num.value} adicionado.`
@@ -34,4 +30,9 @@ function adicionar () {
     }
     num.value = ''
     num.focus()
+}
+
+function finalizar (){
+
+    
 }
